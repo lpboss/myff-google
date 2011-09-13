e2a58de4c84b16ccef85beb5e9552576d5137a96
jerry <jerry@jerry-PC>
2011-09-13 15:23:23 +0800

    [no commit message]

--- src/java/ff/controller/PrivilegeController.java
+++ src/java/ff/controller/PrivilegeController.java
@@ -5,6 +5,8 @@
 package ff.controller;
 
 import ff.service.PrivilegeService;
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -36,10 +38,19 @@ public class PrivilegeController extends MultiActionController {
      *作者：jerry
      *描述：得到某个节点的权限树结构。
      */
-    public ModelAndView getSysPrivilegeChildrenById(HttpServletRequest request, HttpServletResponse response) {
-        logger.info("systemPrivilege page");
-        ModelAndView mav = new ModelAndView();
-        return mav;
+    public void getSysPrivilegeChildrenById(HttpServletRequest request, HttpServletResponse response) {
+        String nodeId = request.getParameter("node");
+        String jsonStr = privilegeService.getSysPrivilegeChildrenById(Long.parseLong(nodeId));
+        PrintWriter pw;
+        try {
+            response.setContentType("text/json; charset=utf-8");
+            response.setHeader("Cache-Control", "no-cache");
+            pw = response.getWriter();
+            pw.write(jsonStr);
+            pw.close();
+        } catch (IOException e) {
+            logger.info(e);
+        }
     }
     
     
