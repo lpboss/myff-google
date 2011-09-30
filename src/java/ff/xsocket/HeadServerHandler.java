f286b4d113cad76be39f497fedb7d96271e3342d
jerrywang1975 <jerrywang1975@gmail.com>
2011-09-30 16:21:12 +0800

    [no commit message]

--- src/java/ff/xsocket/HeadServerHandler.java
+++ src/java/ff/xsocket/HeadServerHandler.java
@@ -17,124 +17,130 @@ import org.xsocket.connection.IServer;
 import org.xsocket.connection.Server;
 
 public class HeadServerHandler implements IDataHandler, IConnectHandler,
-		IIdleTimeoutHandler, IConnectionTimeoutHandler, IDisconnectHandler {
+        IIdleTimeoutHandler, IConnectionTimeoutHandler, IDisconnectHandler {
 
-	/**
-	 * 即当建立完连接之后可以进行的一些相关操作处理。包括修改连接属性、准备资源、等！ 连接的成功时的操作
-	 */
-	@Override
-	public boolean onConnect(INonBlockingConnection connection)
-			throws IOException, BufferUnderflowException,
-			MaxReadSizeExceededException {
+    /**
+     * 即当建立完连接之后可以进行的一些相关操作处理。包括修改连接属性、准备资源、等！ 连接的成功时的操作
+     */
+    @Override
+    public boolean onConnect(INonBlockingConnection connection)
+            throws IOException, BufferUnderflowException,
+            MaxReadSizeExceededException {
 
-		String ip = connection.getRemoteAddress().getHostAddress();
-		ServerUtil.getInstance().addConnection(ip, connection);
+        String ip = connection.getRemoteAddress().getHostAddress();
+        ServerUtil.getInstance().addConnection(ip, connection);
 
-		System.out.println("云台控制客户端(" + ip + ":" + connection.getLocalPort()
-				+ ")已连接！");
+        System.out.println("云台控制客户端(" + ip + ":" + connection.getLocalPort()
+                + ")已连接！");
 
-		// 发送云台水平角度查询命令
-		// ServerUtil.getInstance().sendCommand(connection,
-		// "FF 01 00 04 30 00 35");//水平转动
-		// ServerUtil.getInstance().sendCommand(connection,
-		// "FF 01 00 00 00 00 01");//停止
-		ServerUtil.getInstance()
-				.sendCommand(connection, "FF 01 00 51 00 00 52");
+        // 发送云台水平角度查询命令
+        // ServerUtil.getInstance().sendCommand(connection,
+        // "FF 01 00 04 30 00 35");//水平转动
+        // ServerUtil.getInstance().sendCommand(connection,
+        // "FF 01 00 00 00 00 01");//停止
+        ServerUtil.getInstance().sendCommand(connection, "FF 01 00 51 00 00 52");
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * 即如果失去连接应当如何处理？ 需要实现 IDisconnectHandler 这个接口 连接断开时的操作
-	 */
-	@Override
-	public boolean onDisconnect(INonBlockingConnection connection)
-			throws IOException {
-		if (connection != null && connection.isOpen()) {
-			String ip = connection.getRemoteAddress().getHostAddress();
-			ServerUtil.getInstance().removeConnection(ip);
-			System.out.println("云台控制客户端(" + ip + ":"
-					+ connection.getLocalPort() + ")已断开！");
-		}
-		return false;
-	}
+    /**
+     * 即如果失去连接应当如何处理？ 需要实现 IDisconnectHandler 这个接口 连接断开时的操作
+     */
+    @Override
+    public boolean onDisconnect(INonBlockingConnection connection)
+            throws IOException {
+        if (connection != null && connection.isOpen()) {
+            String ip = connection.getRemoteAddress().getHostAddress();
+            ServerUtil.getInstance().removeConnection(ip);
+            System.out.println("云台控制客户端(" + ip + ":"
+                    + connection.getLocalPort() + ")已断开！");
+        }
+        return false;
+    }
 
-	/**
-	 * 即这个方法不光是说当接收到一个新的网络包的时候会调用而且如果有新的缓存存在的时候也会被调用。而且 当连接被关闭的时候也会被调用的!
-	 */
-	@Override
-	public boolean onData(INonBlockingConnection connection)
-			throws IOException, BufferUnderflowException,
-			ClosedChannelException, MaxReadSizeExceededException {
-		if (connection != null && connection.isOpen()) {
-			String ip = connection.getRemoteAddress().getHostAddress();
+    /**
+     * 即这个方法不光是说当接收到一个新的网络包的时候会调用而且如果有新的缓存存在的时候也会被调用。而且 当连接被关闭的时候也会被调用的!
+     */
+    @Override
+    public boolean onData(INonBlockingConnection connection)
+            throws IOException, BufferUnderflowException,
+            ClosedChannelException, MaxReadSizeExceededException {
+        if (connection != null && connection.isOpen()) {
+            String ip = connection.getRemoteAddress().getHostAddress();
 
-			ByteBuffer buffer = ByteBuffer.allocate(7);
-			connection.read(buffer);
-			byte[] b = buffer.array();
+            ByteBuffer buffer = ByteBuffer.allocate(7);
+            connection.read(buffer);
+            byte[] b = buffer.array();
 
-			String s = ServerUtil.getInstance().byteArray2HexString(b);
-			if (s.indexOf("FF010059") > -1) {
-				float angle_x = (float) Integer
-						.parseInt(s.substring(8, 12), 16) / 100;
-				ServerUtil.getInstance().addHeadInfo(ip, angle_x + "");
+            String s = ServerUtil.getInstance().byteArray2HexString(b);
+            System.out.println(".......................onData+s:" + s);
+            if (s.indexOf("FF010059") > -1) {
+                float angle_x = (float) Integer.parseInt(s.substring(8, 12), 16) / 100;
+                ServerUtil.getInstance().addHeadInfo(ip, angle_x + "");
 
-				//System.out.println(s + "/" + angle_x);
-			}
+                //System.out.println(s + "/" + angle_x);
+            }
 
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-			// 发送云台水平角度查询命令
-			ServerUtil.getInstance().sendCommand(connection,
-					"FF 01 00 51 00 00 52");
-		}
-		return true;
-	}
+            // 发送云台水平角度查询命令
+            ServerUtil.getInstance().sendCommand(connection,
+                    "FF 01 00 51 00 00 52");
+        }
+        return true;
+    }
 
-	/**
-	 * 请求处理超时的处理事件
-	 */
-	@Override
-	public boolean onIdleTimeout(INonBlockingConnection connection)
-			throws IOException {
-		if (connection != null && connection.isOpen()) {
-			String ip = connection.getRemoteAddress().getHostAddress();
-			System.out.println("云台控制客户端(" + ip + ":"
-					+ connection.getLocalPort() + ")请求处理超时！");
-		}
+    /**
+     * 请求处理超时的处理事件
+     */
+    @Override
+    public boolean onIdleTimeout(INonBlockingConnection connection)
+            throws IOException {
+        if (connection != null && connection.isOpen()) {
+            String ip = connection.getRemoteAddress().getHostAddress();
+            System.out.println("云台控制客户端(" + ip + ":"
+                    + connection.getLocalPort() + ")请求处理超时！");
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * 连接超时处理事件
-	 */
-	@Override
-	public boolean onConnectionTimeout(INonBlockingConnection connection)
-			throws IOException {
-		if (connection != null && connection.isOpen()) {
-			String ip = connection.getRemoteAddress().getHostAddress();
-			System.out.println("云台控制客户端(" + ip + ":"
-					+ connection.getLocalPort() + ")连接超时！");
-		}
+    /**
+     * 连接超时处理事件
+     */
+    @Override
+    public boolean onConnectionTimeout(INonBlockingConnection connection)
+            throws IOException {
+        if (connection != null && connection.isOpen()) {
+            String ip = connection.getRemoteAddress().getHostAddress();
+            System.out.println("云台控制客户端(" + ip + ":"
+                    + connection.getLocalPort() + ")连接超时！");
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public static void main(String[] args) throws Exception {
-		IServer srv = null;
-		try {
-			InetAddress address = InetAddress.getByName("192.168.254.111");
-			srv = new Server(8002, new HeadServerHandler());
-			// srv.setConnectionTimeoutMillis(10000);
-			srv.start();
+    public static void main(String[] args) throws Exception {
+        IServer srv = null;
+        try {
+            IServer server_head = null;
+            //启动云台控制服务
+            server_head = new Server(8002, new HeadServerHandler());
+            server_head.setConnectionTimeoutMillis(10000);
+            server_head.setIdleTimeoutMillis(10000);
+            server_head.start();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//            InetAddress address = InetAddress.getByName("192.168.254.175");
+//            srv = new Server(8002, new HeadServerHandler());
+//            // srv.setConnectionTimeoutMillis(10000);
+//            srv.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
