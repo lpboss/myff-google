dd210072511214070037c29541e618ef69596db0
jerrywang1975 <jerrywang1975@gmail.com>
2011-10-09 15:10:41 +0800

    [no commit message]

--- src/java/ff/util/PTZCruiseTask.java
+++ src/java/ff/util/PTZCruiseTask.java
@@ -3,6 +3,9 @@
  * and open the template in the editor.
  */
 package ff.util;
+
+import ff.xsocket.SerialPortCommServer;
+import java.util.Date;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +14,18 @@ import org.springframework.stereotype.Service;
  * @author jerry
  * 此类中的方法，负责执行定时巡航任务。
  */
-
 @Service
 public class PTZCruiseTask {
-    @Scheduled(fixedDelay = 30000)
+    
+    private SerialPortCommServer serialPortCommServer;
+    
+    public void setSerialPortCommServer(SerialPortCommServer serialPortCommServer) {
+        this.serialPortCommServer = serialPortCommServer;
+    }
+    
+    @Scheduled(fixedDelay = 3000)
     public void testTwoPrint() {
-        System.out.println("TestTwo测试打印");
-    }    
+        
+        System.out.println("TestTwo测试打印" + new Date() + ",云台角度：" + serialPortCommServer.getHeadInfo("192.168.254.65"));
+    }
 }
