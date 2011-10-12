38b6daa2c0ab9579a26d0e0719272ce1223ca3aa
jerrywang1975 <jerrywang1975@gmail.com>
2011-10-12 15:36:12 +0800

    [no commit message]

--- src/java/ff/util/PTZCruiseTask.java
+++ src/java/ff/util/PTZCruiseTask.java
@@ -11,8 +11,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import ff.xsocket.SerialPortCommServer;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
@@ -49,7 +50,7 @@ public class PTZCruiseTask {
      *作者：jerry
      *描述：发送云台角度查询命令，命令的结果会在HeadServerHandler的回调方法onData中进行分析，然后放入serialPortCommServer的angleX，angleY二个类变量中。
      */
-    @Scheduled(fixedDelay = 50)
+    @Scheduled(fixedDelay = 400)
     public void sendPTZCommand() {
         // 发送云台角度查询命令
         try {
@@ -58,8 +59,15 @@ public class PTZCruiseTask {
             while (ips.hasNext()) {
                 String ip = ips.next();
                 LinkedList<String> commandQueue = commandMap.get(ip);
-                serialPortCommServer.sendCommand(ip, commandQueue.getFirst());
-                commandQueue.removeFirst();
+                if (commandQueue.size() > 0) {
+                    Calendar calendar = Calendar.getInstance();
+                    long milliseconds = calendar.getTimeInMillis();
+                    SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss SSS");
+                    Date date = new Date(milliseconds);
+                    System.out.println("ip, commandQueue.getFirst():" + ip + ",:" + commandQueue.getFirst() + ",Date:" + timeFormat.format(date));
+                    serialPortCommServer.sendCommand(ip, commandQueue.getFirst());
+                    commandQueue.removeFirst();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -72,7 +80,7 @@ public class PTZCruiseTask {
      */
     @Scheduled(fixedDelay = 200)
     public void PTZCruise() {
-        System.out.println("public void PTZCruise()+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        //System.out.println("public void PTZCruise()+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         //判断所有的云台，如果没有key添加值并巡航，如果有值，则依次判断巡航方向，比如是向上还是向下。如果角度在359度时，则上跳或下跳X度。
         //如果ptzOrientation中无值，默认向下转动云台。
         //暂时，只是为只有一个平台，即IP为：192.168.254.65
@@ -86,15 +94,15 @@ public class PTZCruiseTask {
 
         } else {
             //如果云台巡航有相关标志参数。则判断参数的值。
-            System.out.println("serialPortCommServer.getAllowCruise() == " + serialPortCommServer.getAllowCruise().get("192.168.254.65") + ",Time+" + new Date());
-            System.out.println("serialPortCommServer.angleX:" + serialPortCommServer.getAngleX("192.168.254.65") + ", angleY :" + serialPortCommServer.getAngleY("192.168.254.65"));
+            //System.out.println("serialPortCommServer.getAllowCruise() == " + serialPortCommServer.getAllowCruise().get("192.168.254.65") + ",Time+" + new Date());
+            //System.out.println("serialPortCommServer.angleX:" + serialPortCommServer.getAngleX("192.168.254.65") + ", angleY :" + serialPortCommServer.getAngleY("192.168.254.65"));
             if (serialPortCommServer.getAllowCruise().get("192.168.254.65") == Boolean.TRUE) {
 
                 //TODO:如何判断云台当前是否正在转动？因无法判断，当前每次发送转动命令。
                 serialPortCommServer.pushCommand("192.168.254.65", "FF 01 00 02 10 00 13");
                 //发命令，读云台角度
-                serialPortCommServer.pushCommand("192.168.254.65", "FF 01 00 51 00 00 52 FF 01 00 53 00 00 54");
-
+                serialPortCommServer.pushCommand("192.168.254.65", "FF 01 00 51 00 00 52");
+                serialPortCommServer.pushCommand("192.168.254.65", "FF 01 00 53 00 00 54");
             }
         }
         //判断当前云台是否有旋转方向的标记，如果没有则默认设置向下。
