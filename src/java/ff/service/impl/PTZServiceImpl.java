8887b88eef51e9cae9ec1111b541e3daccf810e4
jerrywang1975 <jerrywang1975@gmail.com>
2011-11-09 18:00:13 +0800

    [no commit message]

---
+++ src/java/ff/service/impl/PTZServiceImpl.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ff.service.impl;
+
+import ff.service.PTZService;
+
+/**
+ *
+ * @author jerry
+ */
+public class PTZServiceImpl implements PTZService{
+    
+}
