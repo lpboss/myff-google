318e476ffac3b03990ed95473fee6786e9c5db93
jerrywang1975 <jerrywang1975@gmail.com>
2011-11-16 18:34:06 +0800

    [no commit message]

---
+++ src/java/ff/service/SysControllerService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ff.service;
+
+import ff.model.RolesPrivilegeDetail;
+import ff.model.SysController;
+import java.util.List;
+
+/**
+ *
+ * @author Joey
+ */
+public interface SysControllerService {
+
+    String getAllSysControllers();
+    
+    SysController getSysControllerById(Long id);
+    
+    SysController saveOrUpdate(SysController sysController);
+    
+}
---
+++ src/java/ff/service/impl/PrivilegeServiceImpl.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ff.service.impl;
+
+import ff.dao.PrivilegeDao;
+
+import ff.dao.RolesPrivilegeDetailDao;
+import ff.model.Privilege;
+import ff.model.PrivilegeDetail;
+import ff.service.PrivilegeService;
+import ff.util.DateJsonValueProcessor;
+import java.sql.Timestamp;
+import java.util.List;
+import net.sf.json.JSONArray;
+import net.sf.json.JsonConfig;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author jerry
+ */
+public class PrivilegeServiceImpl implements PrivilegeService {
+
+    static Logger logger = Logger.getLogger(PrivilegeServiceImpl.class.getName());
+    //注入区：
+    private PrivilegeDao privilegeDao;
+    private RolesPrivilegeDetailDao rolesPrivilegeDetailDao;
+
+    public void setPrivilegeDao(PrivilegeDao privilegeDao) {
+        this.privilegeDao = privilegeDao;
+    }
+
+    public void setRolesPrivilegeDetailDao(RolesPrivilegeDetailDao rolesPrivilegeDetailDao) {
+        this.rolesPrivilegeDetailDao = rolesPrivilegeDetailDao;
+    }
+
+    @Override
+    public String getSysPrivilegeChildrenById(Long nodeId, Integer isLocked) {
+        return privilegeDao.getSysPrivilegeChildrenById(nodeId, isLocked);
+    }
+
+    @Override
+    public Privilege getPrivilegeById(Long id) {
+        return privilegeDao.getPrivilegeById(id);
+    }
+
+    @Override
+    public String getPrivilegeJSONById(Long id) {
+        Privilege privilege = privilegeDao.getPrivilegeById(id);
+        JsonConfig jsonConfig = new JsonConfig();
+        //这是需要过滤掉的变量名。
+        //jsonConfig.setExcludes(new String[]{"videos", "users", "role_monitors"});
+        jsonConfig.registerJsonValueProcessor(Timestamp.class, new DateJsonValueProcessor("yyyy-MM-dd HH:mm"));
+        JSONArray privilegeJS = JSONArray.fromObject(privilege, jsonConfig);
+        String jsonStr = "{root:" + privilegeJS.toString() + "}";
+        return jsonStr;
+    }
+
+    @Override
+    public String getAllModulesJSON() {
+        List<Privilege> privileges = privilegeDao.getAllModules();
+        JsonConfig jsonConfig = new JsonConfig();
+        //这是需要过滤掉的变量名。
+        //jsonConfig.setExcludes(new String[]{"videos", "users", "role_monitors"});
+        jsonConfig.registerJsonValueProcessor(Timestamp.class, new DateJsonValueProcessor("yyyy-MM-dd HH:mm"));
+        JSONArray privilegesJS = JSONArray.fromObject(privileges, jsonConfig);
+        String jsonStr = "{totalProperty:" + privileges.size() + ",root:" + privilegesJS.toString() + "}";
+        return jsonStr;
+    }
+
+    @Override
+    public Privilege saveOrUpdate(Privilege privilege) {
+        return privilegeDao.saveOrUpdate(privilege);
+    }
+
+    @Override
+    public Privilege getPrivilegeByParentIdSortId(Long parentId, Integer sortId) {
+        return privilegeDao.getPrivilegeByParentIdSortId(parentId, sortId);
+    }
+
+    @Override
+    public Integer getMaxSortIdByParentId(Long parentId) {
+        return privilegeDao.getMaxSortIdByParentId(parentId);
+    }
+
+    //同时在删除权限时，要同时删除所有角色的相关权限
+    @Override
+    public String delete(Long id) {
+        privilegeDao.delete(id);
+        rolesPrivilegeDetailDao.deleteForSysPrivilegeDelete(id);
+        return "success";
+    }
+}
