85015b3fa2b93078d9f302f8a4ef168946a47c18
hao76521 <hao76521@gmail.com>
2011-11-18 08:36:07 +0800

    [no commit message]

--- src/java/ff/controller/PTZController.java
+++ src/java/ff/controller/PTZController.java
@@ -119,15 +119,15 @@ public class PTZController extends MultiActionController {
         ptz.setInfraredRTSPUrl(request.getParameter("infrared_rtsp_url"));//红外RTSP流
         ptz.setInfraredCameraUrl(request.getParameter("infrared_camera_url"));//红外摄像机地址
         ptz.setInfraredCircuitUrl(request.getParameter("infrared_circuit_url"));//红外电路板设备地址       
-        ptz.setNorthMigration(request.getIntHeader("north_migration"));//摄像机0角度与正北的偏移
+        ptz.setNorthMigration(Float.valueOf(request.getParameter("north_migration")));//摄像机0角度与正北的偏移
         ptz.setGisMapUrl(request.getParameter("gis_map_url"));//地图文件存放位置
-        ptz.setVisualAngleX(request.getIntHeader("visual_angle_x"));//红外视角X
-        ptz.setVisualAngleY(request.getIntHeader("visual_angle_y"));//红外视角Y
-        ptz.setInfraredPixelX(request.getIntHeader("infrared_pixel_x"));//红外摄像机X方向像素
-        ptz.setInfraredPixelY(request.getIntHeader("infrared_pixel_y"));//红外摄像机Y方向像素    
+        ptz.setVisualAngleX(Float.valueOf(request.getParameter("visual_angle_x")));//红外视角X
+        ptz.setVisualAngleY(Float.valueOf(request.getParameter("visual_angle_y")));//红外视角Y
+        ptz.setInfraredPixelX(Integer.valueOf(request.getParameter("infrared_pixel_x")));//红外摄像机X方向像素
+        ptz.setInfraredPixelY(Integer.valueOf(request.getParameter("infrared_pixel_y")));//红外摄像机Y方向像素    
         ptz.setBrandType(request.getParameter("brand_type"));//品牌类型,不同品牌，特性不同，plcod命令拼接方式不同。
-        ptz.setCruiseStep(Integer.getInteger("cruise_step"));//巡航步长
-        ptz.setVersion(Integer.getInteger("version"));//版本
+        ptz.setCruiseStep(Integer.valueOf(request.getParameter("cruise_step")));//巡航步长
+        ptz.setVersion(Integer.valueOf(request.getParameter("version")));//版本
         ptz.setIsLocked(Long.getLong("isLocked"));//状态
         ptzService.saveOrUpdate(ptz);
         String info = "success";
