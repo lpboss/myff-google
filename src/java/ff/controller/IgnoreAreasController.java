9e8f5f837517764e01d3e9fee33b79df68d61234
hao76521 <hao76521@gmail.com>
2011-11-22 14:47:21 +0800

    [no commit message]

--- src/java/ff/controller/IgnoreAreasController.java
+++ src/java/ff/controller/IgnoreAreasController.java
@@ -18,8 +18,8 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
  *
  * @author Administrator
  */
-public class IgnoreAreasController extends MultiActionController{
-    
+public class IgnoreAreasController extends MultiActionController {
+
     private IgnoreAreasService ignoreAreasService;
 
     public IgnoreAreasService getIgnoreAreasService() {
@@ -29,14 +29,13 @@ public class IgnoreAreasController extends MultiActionController{
     public void setIgnoreAreasService(IgnoreAreasService ignoreAreasService) {
         this.ignoreAreasService = ignoreAreasService;
     }
-    
 
     //返回报警忽视地区页面
     public ModelAndView ignoreAreasList(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView mav = new ModelAndView();
         return mav;
     }
-    
+
     //返回 添加页面
     public ModelAndView newIgnoreAreas(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView mav = new ModelAndView();
@@ -48,15 +47,16 @@ public class IgnoreAreasController extends MultiActionController{
         ModelAndView mav = new ModelAndView();
         return mav;
     }
-    
+
     //得到报警忽视地区信息
     public void getAllIgnoreAreases(HttpServletRequest request, HttpServletResponse response) {
         String id = request.getParameter("id");
         logger.info("88888888888");
         logger.info(id);
-        String jsonStr = ignoreAreasService.getIgnoreAreasJSONById(Long.parseLong(request.getParameter("id")));
-     //   String jsonStr = ignoreAreasService.getIgnoreAreasList();
+        String jsonStr = ignoreAreasService.getIgnoreAreasJSONById(Integer.parseInt(request.getParameter("id")));
+        //   String jsonStr = ignoreAreasService.getIgetIgnoreAreasJSONByIdgnoreAreasList();
         logger.info(jsonStr);
+        logger.info("666666666");
         PrintWriter pw;
         try {
             response.setContentType("text/json; charset=utf-8");
@@ -68,11 +68,11 @@ public class IgnoreAreasController extends MultiActionController{
             logger.info(e);
         }
     }
-    
+
     //添加报警忽视地区信息
     public void create(HttpServletRequest request, HttpServletResponse response) {
-     //   String id = request.getParameter("id");
-        IgnoreAreas ignoreAreas = new IgnoreAreas();  
+        //   String id = request.getParameter("id");
+        IgnoreAreas ignoreAreas = new IgnoreAreas();
         ignoreAreas.setPtzId(Integer.valueOf(request.getParameter("ptz_id"))); //云台ID
         ignoreAreas.setPtzAngelX(Integer.valueOf(request.getParameter("ptz_angel_x"))); //火警时云台的水平角度       
         ignoreAreas.setPtzAngelY(Integer.valueOf(request.getParameter("ptz_angel_y"))); //火警时云台的Y角度
@@ -96,10 +96,10 @@ public class IgnoreAreasController extends MultiActionController{
             logger.info(e);
         }
     }
-    
+
     //编辑AllAlarmIgnoreAreases
     public void getIgnoreAreasesById(HttpServletRequest request, HttpServletResponse response) {
-        Long id = Long.valueOf(request.getParameter("id"));
+        Integer id = Integer.valueOf(request.getParameter("id"));
         String jsonStr = ignoreAreasService.getIgnoreAreasJSONById(id);
         PrintWriter pw;
         try {
@@ -112,10 +112,10 @@ public class IgnoreAreasController extends MultiActionController{
             logger.info(e);
         }
     }
-    
-     //更新报警忽视地区
+
+    //更新报警忽视地区
     public void update(HttpServletRequest request, HttpServletResponse response) {
-        Long id = Long.valueOf(request.getParameter("id"));
+        Integer id = Integer.valueOf(request.getParameter("id"));
         Integer ptzAngelX = Integer.valueOf(request.getParameter("ptz_angel_x")); //火警时云台的水平角度    
         Integer ptzAngelY = Integer.valueOf(request.getParameter("ptz_angel_y")); //火警时云台的Y角度
         Integer ccdArea = Integer.valueOf(request.getParameter("ccd_area")); //热成像起火面积值
@@ -124,7 +124,7 @@ public class IgnoreAreasController extends MultiActionController{
         Timestamp endDate = Timestamp.valueOf(request.getParameter("end_date")); //火警时间范围(结束)
         Integer version = Integer.valueOf(request.getParameter("version")); //版本
         Long isLocked = Long.valueOf(request.getParameter("is_locked"));//状态
-     //   Long ptzId = Long.valueOf(request.getParameter("roleId"));
+        //   Long ptzId = Long.valueOf(request.getParameter("roleId"));
         IgnoreAreas ignoreAreas = ignoreAreasService.getIgnoreAreasById(id);
         ignoreAreas.setId(id);
         ignoreAreas.setPtzAngelX(ptzAngelX);
@@ -135,8 +135,8 @@ public class IgnoreAreasController extends MultiActionController{
         ignoreAreas.setEndDate(endDate);
         ignoreAreas.setVersion(version);
         ignoreAreas.setIsLocked(isLocked);
-       // ignoreAreas.setIsLocked(Long.valueOf(isLocked));
-      //  logger.info(ptzId);
+        // ignoreAreas.setIsLocked(Long.valueOf(isLocked));
+        //  logger.info(ptzId);
         PrintWriter pw;
         try {
             logger.info("user update..............................................Begin..........");
@@ -152,15 +152,15 @@ public class IgnoreAreasController extends MultiActionController{
         } catch (IOException e) {
             logger.info(e);
         }
-    } 
-    
-     //删除报警忽视地区
+    }
+
+    //删除报警忽视地区
     public void deleteIgnoreAreas(HttpServletRequest request, HttpServletResponse response) {
         Long id = Long.valueOf(request.getParameter("key"));
-        logger.info ("sss");
-        logger.info (id);
-        logger.info ("www");
-      String jsonStr = ignoreAreasService.deleteIgnoreAreas(id);
+        logger.info("sss");
+        logger.info(id);
+        logger.info("www");
+        String jsonStr = ignoreAreasService.deleteIgnoreAreas(id);
         PrintWriter pw;
         try {
             response.setContentType("text/json; charset=utf-8");
@@ -172,10 +172,4 @@ public class IgnoreAreasController extends MultiActionController{
             logger.info(e);
         }
     }
-    
-    
-    
-    
-    
-    
 }
