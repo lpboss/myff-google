ab0704848129d5fe0fef6caaf37ad820498585d7
hao76521 <hao76521@gmail.com>
2011-11-23 08:59:59 +0800

    [no commit message]

---
+++ src/java/ff/service/AdminService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ff.service;
+
+/**
+ *
+ * @author jerry
+ */
+public interface AdminService {
+
+    Integer executeNativeUpdateSQL(String sql);
+
+    //因为有写入操作，方便事务的情况叫，叫do
+    String doRolePrivilegeDetailsById(Long roleId, Long privilegeId, Long parentPrivilegeId);
+    
+    String rolePrivilegeDetailLock(Long id);
+    
+}
---
+++ src/java/ff/service/FireAlarmService.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ff.service;
+
+import ff.model.FireAlarm;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface FireAlarmService {
+
+    String getFireAlarmList(Integer ptzId, Timestamp beginTime, Timestamp endTime);
+
+    String create(FireAlarm fireAlarm);
+
+    String deleteFireAlarm(String id);
+
+    String getFireAlarmJSONById(Long id);
+
+    String update(FireAlarm fireAlarm);
+
+    FireAlarm getFireAlarmById(Long id);
+
+    public String fireAlarmLock(FireAlarm fireAlarm);
+}
